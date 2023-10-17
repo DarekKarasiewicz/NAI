0dc9d8c698d846f9f6fcfd850e81719821e91f16
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2023-10-17 17:55:34 +0200

    WIP

--- main.py
+++ main.py
@@ -7,22 +7,22 @@ from skfuzzy import control as ctrl
 
 # New Antecedent/Consequent objects hold universe variables and membership
 # functions
-WindSpeed = ctrl.Antecedent(np.arange(0, 50, 1), 'WindSpeed ')
-BoardLength = ctrl.Antecedent(np.arange(132, 166, 1), 'BoardLength') #długość w cm
-BoardWeight = ctrl.Consequent(np.arange(39, 50, 1), 'BoardWeight')  #szerokość w cm
+wind_speed = ctrl.Antecedent(np.arange(0, 60, 1), 'WindSpeed ')
+board_length = ctrl.Antecedent(np.arange(132, 166, 1), 'BoardLength') #długość w cm
+board_width = ctrl.Consequent(np.arange(39, 50, 1), 'BoardWeight')  #szerokość w cm
 # można dać też wagę
-KiteSize = ctrl.Consequent(np.arange(2, 15, 1), 'KiteSize')
+kite_size = ctrl.Consequent(np.arange(2, 15, 1), 'KiteSize')
 
 
 # Auto-membership function population is possible with .automf(3, 5, or 7)
-WindSpeed.automf(5)
-BoardLength.automf(5)
-BoardWeight.automf(5)
+wind_speed.automf(5)
+board_length.automf(5)
+board_width.automf(5)
 
 # Custom membership functions can be built interactively with a familiar,
 # Pythonic API
-KiteSize['very small'] = fuzz.trimf(KiteSize.universe, [3, 6, 9])
-KiteSize['small'] = fuzz.trimf(KiteSize.universe, [6, 9, 12])
-KiteSize['medium'] = fuzz.trimf(KiteSize.universe, [9, 12, 15])
-KiteSize['large'] = fuzz.trimf(KiteSize.universe, [12, 15, 18])
-KiteSize['very large'] = fuzz.trimf(KiteSize.universe, [15, 18, 18])
+kite_size['very small'] = fuzz.trimf(kite_size.universe, [3, 6, 9])
+kite_size['small'] = fuzz.trimf(kite_size.universe, [6, 9, 12])
+kite_size['medium'] = fuzz.trimf(kite_size.universe, [9, 12, 15])
+kite_size['large'] = fuzz.trimf(kite_size.universe, [12, 15, 18])
+kite_size['very large'] = fuzz.trimf(kite_size.universe, [15, 18, 18])
