9974562f7900d939e7ce362f1471fb57043d37f1
Mikolajkusinski <77103765+Mikolajkusinski@users.noreply.github.com>
2023-11-20 18:08:05 +0100

    Dodane wczytywanie danych z pliku oraz obliczanie Dokładności Drzewa oraz SVM do pierwszego zestawu danych

---
+++ Zjazd4/main.py
@@ -0,0 +1,27 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
+from sklearn.tree import DecisionTreeClassifier
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
+
+
+data_array = np.loadtxt('seeds_dataset.txt')
+
+X = data_array[:, :-1]
+y = data_array[:, -1]
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+clf_tree = DecisionTreeClassifier()
+clf_tree.fit(X_train, y_train)
+y_pred_tree = clf_tree.predict(X_test)
+accuracy_tree = accuracy_score(y_test, y_pred_tree)
+print(f'Dokładność Drzewa Decyzyjnego: {accuracy_tree}')
+
+# SVM
+clf_svm = SVC()
+clf_svm.fit(X_train, y_train)
+y_pred_svm = clf_svm.predict(X_test)
+accuracy_svm = accuracy_score(y_test, y_pred_svm)
+print(f'Dokładność SVM: {accuracy_svm}')
