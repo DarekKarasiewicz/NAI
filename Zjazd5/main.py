a60b5594921e3d1771fadfef0b08b6e44f1da5ac
Darek Karasiewicz <dar.karasiewicz@gmail.com>
2023-12-08 13:41:22 +0100

    Zjazd5 zad4

--- Zjazd5/main.py
+++ Zjazd5/main.py
@@ -220,6 +220,115 @@ def fashion():
   plt.tight_layout()
   plt.show()
 
+def MINST():
+  def load_mnist(path, kind='train'):
+      import os
+      import gzip
+      import numpy as np
 
-animals()
-fashion()
+      labels_path = os.path.join(path, f'{kind}-labels-idx1-ubyte.gz')
+      images_path = os.path.join(path, f'{kind}-images-idx3-ubyte.gz')
+
+      with gzip.open(labels_path, 'rb') as lbpath:
+          labels = np.frombuffer(lbpath.read(), dtype=np.uint8, offset=8)
+
+      with gzip.open(images_path, 'rb') as imgpath:
+          images = np.frombuffer(imgpath.read(), dtype=np.uint8, offset=16).reshape(len(labels), 28, 28)
+
+      return images, labels
+
+  # Podaj ścieżkę do folderu zawierającego pliki danych
+  path = './MINST'
+
+  # Wczytaj dane treningowe
+  train_images, train_labels = load_mnist(path, kind='train')
+
+  # Wczytaj dane testowe
+  test_images, test_labels = load_mnist(path, kind='t10k')
+
+  # Reskaluj obrazy do zakresu [0, 1]
+  train_images = train_images / 255.0
+  test_images = test_images / 255.0
+
+  # Wyświetl kilka obrazów treningowych
+  plt.figure(figsize=(10, 10))
+  for i in range(25):
+      plt.subplot(5, 5, i + 1)
+      plt.xticks([])
+      plt.yticks([])
+      plt.grid(False)
+      plt.imshow(train_images[i], cmap=plt.cm.binary)
+      plt.xlabel(str(train_labels[i]))
+  plt.show()
+
+  # Zbuduj model
+  model = tf.keras.Sequential([
+      tf.keras.layers.Flatten(input_shape=(28, 28)),
+      tf.keras.layers.Dense(128, activation='relu'),
+      tf.keras.layers.Dense(10)
+  ])
+
+  # Kompiluj model
+  model.compile(optimizer='adam',
+                loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+                metrics=['accuracy'])
+
+  # Trenuj model
+  model.fit(train_images, train_labels, epochs=10)
+
+  # Ocena modelu na zbiorze testowym
+  test_loss, test_acc = model.evaluate(test_images, test_labels, verbose=2)
+  print('\nTest accuracy:', test_acc)
+
+  # Utwórz model prawdopodobieństwa dla predykcji
+  probability_model = tf.keras.Sequential([model, tf.keras.layers.Softmax()])
+  predictions = probability_model.predict(test_images)
+
+  # Funkcje do wizualizacji
+  def plot_image(i, predictions_array, true_label, img):
+      true_label, img = true_label[i], img[i]
+      plt.grid(False)
+      plt.xticks([])
+      plt.yticks([])
+
+      plt.imshow(img, cmap=plt.cm.binary)
+
+      predicted_label = np.argmax(predictions_array)
+      if predicted_label == true_label:
+          color = 'blue'
+      else:
+          color = 'red'
+
+      plt.xlabel("{} {:2.0f}% ({})".format(predicted_label,
+                                            100 * np.max(predictions_array),
+                                            true_label),
+                color=color)
+
+  def plot_value_array(i, predictions_array, true_label):
+      true_label = true_label[i]
+      plt.grid(False)
+      plt.xticks(range(10))
+      plt.yticks([])
+      thisplot = plt.bar(range(10), predictions_array, color="#777777")
+      plt.ylim([0, 1])
+      predicted_label = np.argmax(predictions_array)
+
+      thisplot[predicted_label].set_color('red')
+      thisplot[true_label].set_color('blue')
+
+  # Wizualizacja predykcji
+  num_rows = 5
+  num_cols = 3
+  num_images = num_rows * num_cols
+  plt.figure(figsize=(2 * 2 * num_cols, 2 * num_rows))
+  for i in range(num_images):
+      plt.subplot(num_rows, 2 * num_cols, 2 * i + 1)
+      plot_image(i, predictions[i], test_labels, test_images)
+      plt.subplot(num_rows, 2 * num_cols, 2 * i + 2)
+      plot_value_array(i, predictions[i], test_labels)
+  plt.tight_layout()
+  plt.show()
+
+# animals()
+# fashion()
+MINST()
